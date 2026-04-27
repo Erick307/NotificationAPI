8050fcf835836b1beddb883356ab81e0415475d1
Erick307 <ericksilva1988@gmail.com>
2026-04-27 16:18:05 -0300

    Add ErrorHandling

---
+++ src/main/java/com/ericksilva/notificationapi/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.ericksilva.notificationapi.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Objects;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> jojofrula(MethodArgumentNotValidException exception){
+        String message = Objects.requireNonNull(exception
+                        .getBindingResult()
+                        .getFieldError())
+                .getDefaultMessage();
+
+        ErrorResponse errorResponse = new ErrorResponse(400, message);
+        return ResponseEntity.status(400).body(errorResponse);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGenericException(){
+        ErrorResponse errorResponse = new ErrorResponse(500,"Internal server error");
+        return ResponseEntity.status(500).body(errorResponse);
+    }
+}
